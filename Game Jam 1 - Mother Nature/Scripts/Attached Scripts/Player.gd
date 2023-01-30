c57daa6f34264eeffc9d86b1cece63e750b6952a
unknown <samuelalala126@gmail.com>
2023-01-29 22:53:26 -0300

    First Commit

--- Game Jam 1 - Mother Nature/Scripts/Attached Scripts/Player.gd
+++ Game Jam 1 - Mother Nature/Scripts/Attached Scripts/Player.gd
@@ -1,11 +1,25 @@
 extends KinematicBody2D
 
-const ACCELERATION = 200
+const ACCELERATION : int = 200
+const MAX_SPEED : int = 100
 
+var move_direction = [input_vector.x > 0, input_vector.y > 0]
+
+var input_vector := Vector2.ZERO
 var velocity = Vector2.ZERO
 
 func _physics_process(delta):
-	pass
+	move()
 	
 func move():
-	pass
+	
+	var action_pressed := ""
+	
+	if velocity != Vector2.ZERO:
+		
+		match move_direction:
+			
+			[true, true]: #Direita
+				pass
+	
+	move_and_collide(velocity)
